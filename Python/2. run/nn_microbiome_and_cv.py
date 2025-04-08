318bd87a7b5f0248baccd58601941bfae7c84afb
Yagmur Comba <161379589+yagmurcom@users.noreply.github.com>
2025-04-08 16:14:30 -0400

    Update nn_microbiome_and_cv.py

--- Python/2. run/nn_microbiome_and_cv.py
+++ Python/2. run/nn_microbiome_and_cv.py
@@ -1,27 +1,4 @@
-#Only change from the nn_microbiome is the input data. We now use the both microbiome at species level and clinical variables combined. 
 
-X_combined = pd.concat([df_meta_encoded, df_species], axis=1)
-scaler = MinMaxScaler()
-X_combined_scaled = scaler.fit_transform(X_combined)
-X_combined_scaled = pd.DataFrame(X_combined_scaled)
-X_combined_scaled.columns = X_combined.columns
-X_combined_scaled
-
-def create_model_microbiome(neurons=64, dropout_rate=0.5):
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(neurons, activation='relu', input_shape=(X_combined_scaled.shape[1],)),
-        tf.keras.layers.Dropout(dropout_rate),
-        tf.keras.layers.Dense(neurons // 2, activation='relu'),
-        tf.keras.layers.Dropout(dropout_rate),
-        tf.keras.layers.Dense(1, activation='sigmoid')  
-    ])
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['AUC'])
-    return model
-
-model = KerasClassifier(model=create_model_microbiome, epochs=50, verbose=0,class_weight=class_weights_dict,callbacks=[callback])
-
-param_grid_microbiome = {
-    'model__dropout_rate': [0, 0.2, 0.4, 0.6]}
 
 random_seed_trials = 10 
 

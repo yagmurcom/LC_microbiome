1cc7228aa7598491090b40f708b3702a4f28d2cd
Yagmur Comba <161379589+yagmurcom@users.noreply.github.com>
2025-04-08 16:10:27 -0400

    Update preprocessing.py

--- Python/1. lib/preprocessing.py
+++ Python/1. lib/preprocessing.py
@@ -14,12 +14,6 @@ print(df_meta.isna().any())
 
 df_meta_encoded = pd.get_dummies(df_meta, columns=['ppi','sex','immunosuppression',"fully_vac_index", 'dis_sev', 'DM', 'rend', 'cpd', 'ibd', 'metacanc', 'mi', 'msld', 'transplant_hx','rheumd', 'steroids', 'cancer_chemo', 'calcineurin_inh', 'antimetabolites', 'biologics', 'antiproliferative_agents', 'other'], drop_first=True)
 
-import numpy as np
-X_meta = df_meta_encoded
-y = labels
-scaler = MinMaxScaler()
-X_scaled = scaler.fit_transform(X_meta)
-
 #Train test split
 X_train, X_test, y_train, y_test = train_test_split(X_scaled , y, test_size=0.3, random_state=42,stratify=y)
 
@@ -27,5 +21,3 @@ X_train, X_test, y_train, y_test = train_test_split(X_scaled , y, test_size=0.3,
 unique_classes = np.unique(y_train)
 class_weights = compute_class_weight('balanced', classes=unique_classes, y=y_train.values.reshape(-1))
 class_weights_dict = dict(enumerate(class_weights))
-
-

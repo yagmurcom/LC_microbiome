d2cc69bc621075b70d88806d37d8a6e1c8cdada4
Yagmur Comba <161379589+yagmurcom@users.noreply.github.com>
2025-04-08 16:11:32 -0400

    Update logit_microbiome.py

--- Python/2. run/logit_microbiome.py
+++ Python/2. run/logit_microbiome.py
@@ -1,7 +1,10 @@
-X_species = df_species
 y = labels
-scaler = MinMaxScaler()
-X_scaled_species = scaler.fit_transform(X_species)
+X_species_no_zero = df_species.replace(0, np.finfo(float).eps)
+log_X = np.log(X_species_no_zero)
+row_means = log_X.mean(axis=1)
+X_clr = log_X.subtract(row_means, axis=0)
+X_clr = pd.DataFrame(X_clr, columns=df_species.columns)
+X_clr_scaled = scaler.fit_transform(X_clr)
 
 #Nested CV to optimize on accuracy - SCALED microbiome only
 nested_scores_microbiome_accuracy = np.zeros((random_seed_trials,5))
@@ -10,7 +13,7 @@ for i in range(random_seed_trials):
     outer_cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=i)
     logit_unfitted = LogisticRegression(class_weight=class_weights_dict, random_state=42, solver='liblinear')
     grid_search = GridSearchCV(logit_unfitted, param_grid, cv=inner_cv, scoring='accuracy')
-    cv_scores = cross_val_score(grid_search, X=X_scaled_species, y=y, cv=outer_cv, scoring='accuracy')
+    cv_scores = cross_val_score(grid_search, X=X_clr_scaled, y=y, cv=outer_cv, scoring='accuracy')
     nested_scores_microbiome_accuracy[i] = cv_scores
     print(f"Trial {i+1} - accuracy scores: {cv_scores}")
 
